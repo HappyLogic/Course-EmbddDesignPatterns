1253dd5454b3ee421137e9d617e703541adcc6ae
μςαⓂα <29387130+usa-m@users.noreply.github.com>
2024-05-16 14:02:08 +0500

    Update MotorControl.h

--- Src/03. First Things First/Exercise-Soln/MotorControl.h
+++ Src/03. First Things First/Exercise-Soln/MotorControl.h
@@ -2,8 +2,8 @@
 #define _MY_EXERCISE_SOLN_MOTOR_CONTROL_H
 
 
-#define MOTOR_SWITCH       4
-#define MOTOR_DRIVE        5
+#define MOTOR_SWITCH       4  /* The pin for taking Motor Switch input         */
+#define MOTOR_DRIVE        5  /* The pin for activating and deactivating motor */
 
 void setupMotorPins();
 void updateMotorState();
